fe33ad71bc86468c719d4271e2808adf4cd89330
StandOut <StandOut.first@gmail.com>
2023-04-17 15:00:02 +0900

    호출스택, 가상의 전역 컨텍스트 Anonymous 위로 차곡차곡 쌓인다.

---
+++ 호출스택.js
@@ -0,0 +1,20 @@
+//호출스택
+
+//third-second-first
+//Anonymous: 가상의 전역 컨텍스트 
+
+function first(){
+    second()
+    console.log("첫번째");
+}
+
+function second(){
+    third();
+    console.log("두번째");
+}
+
+function third(){
+    console.log("세번째");
+}
+
+first();
