d07cb863d40a6308d9debfa6548bcd1e235b2cfa
StandOut <StandOut.first@gmail.com>
2023-04-18 14:41:00 +0900

    포트번호를 다르게 지정하여 여러개의 서버 실행하기

--- Node http 서버/createServer.js
+++ Node http 서버/createServer.js
@@ -8,7 +8,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-  res.write('<h1>Hello Node!</h1>');//write 응답 내용
+  res.write('<h1>[8080] Hello Node!</h1>');//write 응답 내용
   res.end('<p>Hello Server!</p>');//end 응답마무리
 });
 // .listen(8080, () => { // 서버 연결
@@ -26,3 +26,14 @@ server.on('listening', () => {
 server.on('error', (error) => {
   console.error(error);
 });
+
+
+//서버추가
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.write('<h1>[8081] Hello Node!</h1>');
+  res.end('<p>Hello Server!</p>');
+})
+  .listen(8081, () => { // 서버 연결
+    console.log('8081번 포트에서 서버 대기 중입니다!');
+  });
