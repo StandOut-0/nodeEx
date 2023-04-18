913c075048b15904b40b3f3f151d74e319d121be
StandOut <StandOut.first@gmail.com>
2023-04-18 14:38:19 +0900

    on(listening, error), 이벤트 리스터 붙이기

--- Node http 서버/createServer.js
+++ Node http 서버/createServer.js
@@ -10,7 +10,19 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
   res.write('<h1>Hello Node!</h1>');//write 응답 내용
   res.end('<p>Hello Server!</p>');//end 응답마무리
-})
-.listen(8080, () => { // 서버 연결
+});
+// .listen(8080, () => { // 서버 연결
+//   console.log('8080번 포트에서 서버 대기 중입니다!');
+// });
+
+server.listen(8080);// 서버 연결
+
+// listening 이벤트 리스너
+server.on('listening', () => {
   console.log('8080번 포트에서 서버 대기 중입니다!');
+});
+
+// error 이벤트 리스너
+server.on('error', (error) => {
+  console.error(error);
 });
