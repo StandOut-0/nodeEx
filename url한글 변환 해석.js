6d716e000a82ce7fffc05225449dee81958d3610
StandOut <StandOut.first@gmail.com>
2023-04-19 15:31:29 +0900

    encodeURIComponent, decodeURIComponent url 한글을 처리/해석하다

---
+++ url한글 변환 해석.js
@@ -0,0 +1,7 @@
+//encodeURIComponent url 한글 처리
+let encodeURI =  encodeURIComponent('노드');
+console.log(encodeURI);
+
+//decodeURIComponent url 인코딩 한글 처리
+let decodeURI =  decodeURIComponent('%EB%85%B8%EB%93%9C');
+console.log(decodeURI);
