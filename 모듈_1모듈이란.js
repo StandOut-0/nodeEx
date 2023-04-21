cd7cf11302eb6c306f1e6cf3ebd4c2d25ee4daa6
StandOut <StandOut.first@gmail.com>
2023-04-21 15:00:57 +0900

    서로 require할 경우에 발생하는 순환참조주의, 순환의존성에 의해 빈객체가 되는 경우

--- 모듈_1모듈이란.js
+++ 모듈_1모듈이란.js
@@ -5,6 +5,11 @@
 const odd = '홀수입니다';
 const even = '짝수입니다';
 
+//순환참조주의, 순환 의존성(circular dependency)
+//만약 이 두 파일이 서로를 require하는 경우, 무한반복을 막기 위해 순환 참조대상이 빈객체가 됨.
+//속성에 엑세스 하려고 하지만 속성이 존재하지않아 오류가 발생
+const checkNumber = require('./모듈_2변수,함수의 모듈화');
+
 //module.exports로 모듈로 만들 값을 지정했다.
 module.exports = {
   odd,
